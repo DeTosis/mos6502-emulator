c75fbdd1c909577e43010d2334011e82ec49f441
DeTosis <92999941+DeTosis@users.noreply.github.com>
2025-08-21 04:01:10 +0500

    fully complete lda

---
+++ README.md
@@ -0,0 +1,3 @@
+# MOS 6502 CPU EMULATOR
+
+> _Currently in development, not much is available at this point_ 
--- main.c
+++ main.c
@@ -2,39 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define byte unsigned char  // 0x00
-#define word unsigned short // 0x0000
+typedef unsigned char byte ;  // 0x00
+typedef unsigned short word ; // 0x0000
 
 /*
     *** MEMORY LAYOUT ***
 */
-#define MAX_RAM         1024 * 64
+#define MAX_RAM          1024 * 64
 
 #define ZERO_PAGE_BEGIN  0x0000
-#define ZERO_PAGE_END   0x00FF
+#define ZERO_PAGE_END    0x00FF
 
-#define STACK_BEGIN     0x00
-#define STACK_END       0xFF
-#define STACK_INIT      0xFD
+#define STACK_BEGIN      0x00
+#define STACK_END        0xFF
+#define STACK_INIT       0xFD
 
-#define ISR_BEGIN       0xFFFA
-#define ISR_END         0xFFFF
+#define ISR_BEGIN        0xFFFA
+#define ISR_END          0xFFFF
 
-#define RESET_LOW       0xFFFC
-#define RESET_HIGH      0xFFFD
+#define RESET_LOW        0xFFFC
+#define RESET_HIGH       0xFFFD
 
-#define NMI_ISR_LOW     ISR_BEGIN
-#define NMI_ISR_HIGH    0xFFFB
+#define NMI_ISR_LOW      ISR_BEGIN
+#define NMI_ISR_HIGH     0xFFFB
 
-#define IRQ_ISR_LOW     0xFFFE
-#define IRQ_ISR_HIGH    ISR_END
+#define IRQ_ISR_LOW      0xFFFE
+#define IRQ_ISR_HIGH     ISR_END
 
 /*
     *** USER SPACE ***
 */
-#define USR_REGION_BEGIN 0x00;
-#define USR_REGION_END  0x02;
-
+#define USR_REGION_BEGIN 0x00
+#define USR_REGION_END   0x02
 
 /*
     *** INSTRUCTIONS ***
@@ -43,7 +42,11 @@
 #define LDA_IMMIDIATE       0xA9    // 2
 #define LDA_ZERO_PAGE       0xA5    // 3
 #define LDA_ZERO_PAGE_X     0xB5    // 4
-#define LDA_ABSOLUTE        0xBD    // 4
+#define LDA_ABSOLUTE        0xAD    // 4
+#define LDA_ABSOLUTE_X      0xBD    // 4 (+1 crossed)
+#define LDA_ABSOLUTE_Y      0xB9    // 4 (+1 crossed)
+#define LDA_INDERECT_X      0xA1    // 6
+#define LDA_INDERECT_Y      0xB1    // 5 (+1 crossed)
 
 typedef struct {
     byte data[MAX_RAM];
@@ -56,7 +59,7 @@ typedef struct{
     byte decimal_flag   : 1;
     byte break_flag     : 1;
     byte overflow_flag  : 1;
-    byte negative_flag   : 1;
+    byte negative_flag  : 1;
 }processor_status;
 
 typedef struct {
@@ -86,15 +89,17 @@ cpu* cpu_routine(ram* r){
     c->ir_x = 0;
     c->ir_y = 0;
     c->program_counter = ram_read_word(r, RESET_LOW);
+    memset(&c->ps, 0, sizeof(c->ps));
     return c;
 }
 
 size_t is_bit_set(byte n, size_t bit){
     if (bit > 7) return 0;
-    return (n >> bit) & 1;
+    int a = (n >> bit) & 1;
+    return a;
 }
 
-// 1 cpu cycle
+// 1 cpu cycles
 byte ram_read_byte(ram* ram, word location){ 
     return ram->data[location];
 }
@@ -104,53 +109,154 @@ word ram_read_word(ram* ram, word location){
     return ram->data[location] | (ram->data[location + 1] << 8);
 }
 
-void cpu_execute(cpu* c, ram* r, int cycles){
-    while (cycles){
-        byte op_code = ram_read_byte(r, c->program_counter++);
-        cycles--;
+// 3 cpu cycles
+word ram_read_word_wrap(ram* ram, word location){
+    return ram->data[location] | (ram->data[(location + 1) & 0xFF] << 8);
+}
 
-        switch (op_code)
-        {
-        case LDA_IMMIDIATE:
-        {
-            c->a = ram_read_byte(r, c->program_counter++);
-            cycles--;
-        }break;
-        case LDA_ZERO_PAGE:
-        {
-            byte addr = ram_read_byte(r, c->program_counter++);
-            cycles--;
-            c->a = ram_read_byte(r, addr);
-            cycles--;
-        }break;
-        case LDA_ZERO_PAGE_X:
-        {
-            //abs location
-            byte addr = ram_read_byte(r, c->program_counter++);
-            cycles--;
-            addr += c->ir_x;
-            cycles--;
-            c->a = ram_read_byte(r, addr);
-            cycles--;
-        }break;
-        case LDA_ABSOLUTE:
-        {
-            //abs location
-            word addr = ram_read_word(r, c->program_counter++);
-            c->a = ram_read_byte(r, addr);
-            c->program_counter++;
-            cycles--;
-            cycles--;
-            cycles--;
-        }break;
-        default:
-            printf("Instruction 0x%x is not implemented yet. \n", op_code);
-            exit(1);
-            break;
-        }
+size_t cpu_process_LDA(cpu* c, ram* r, byte op_code){
+    size_t cycles_used = 0;
+    switch (op_code)
+    {
+    case LDA_IMMIDIATE:
+    {
+        c->a = ram_read_byte(r, c->program_counter++);
+        cycles_used++;
+    }break;
+    case LDA_ZERO_PAGE:
+    {
+        byte zp_addr = ram_read_byte(r, c->program_counter++);
+        cycles_used++;
+        c->a = ram_read_byte(r, zp_addr);
+        cycles_used++;
+    }break;
+    case LDA_ZERO_PAGE_X:
+    {
+        //abs location
+        byte addr = ram_read_byte(r, c->program_counter++);
+        cycles_used++;
+        addr += c->ir_x;
+        cycles_used++;
+        c->a = ram_read_byte(r, addr);
+        cycles_used++;
+    }break;
+    case LDA_ABSOLUTE:
+    {
+        word addr = ram_read_word(r, c->program_counter++);
+        cycles_used++;
+        cycles_used++;
+        c->a = ram_read_byte(r, addr);
+        cycles_used++;
+        c->program_counter++;
+    }break;
+    case LDA_ABSOLUTE_X:
+    {
+        word addr = ram_read_word(r, c->program_counter++);
+        cycles_used++;
+        cycles_used++;
+
+        //cross
+        if ((addr & 0xFF) + c->ir_x >= 0x100)
+            cycles_used++;
+
+        addr += c->ir_x;
+        c->a = ram_read_byte(r, addr);
+        cycles_used++;
+        c->program_counter++;
+    }break;
+    case LDA_ABSOLUTE_Y:
+    {
+        word addr = ram_read_word(r, c->program_counter++);
+        cycles_used++;
+        cycles_used++;
+
+        //cross
+        if ((addr & 0xFF) + c->ir_y >= 0x100)
+            cycles_used++;
+
+        addr += c->ir_y;
+        c->a = ram_read_byte(r, addr);
+        cycles_used++;
+        c->program_counter++;
+    }break;
+    case LDA_INDERECT_X:
+    {
+        byte zp_addr = ram_read_byte(r, c->program_counter++);
+        cycles_used++;
+        zp_addr += c->ir_x;
+        cycles_used++;
+        word addr = ram_read_word_wrap(r, zp_addr);
+        cycles_used++;
+        cycles_used++;
+        c->a = ram_read_byte(r, addr);        
+        cycles_used++;
+    }break;
+    case LDA_INDERECT_Y:
+    {
+        byte zp_addr = ram_read_byte(r, c->program_counter++);
+        cycles_used++;
+        word addr = ram_read_word_wrap(r, zp_addr);
+        cycles_used++;
+        cycles_used++;
+
+        //cross
+        if ((addr & 0xFF) + c->ir_y >= 0x100)
+            cycles_used++;
+        
+        addr += c->ir_y;
+        c->a = ram_read_byte(r, addr);        
+        cycles_used++;
+    }break;
+    default:
+        printf("Instruction 0x%x is not an LDA instruction. \n", op_code);
+        break;
     }
-    c->ps.zero_flag = c->a == 0 ? 0 : 1;
+    return cycles_used;
+}
+
+size_t cpu_execute(cpu* c, ram* r) {
+    size_t cycles_used = 0;
+    byte op_code = ram_read_byte(r, c->program_counter++);
+    cycles_used++;
+
+    switch(op_code)
+    {
+    case LDA_IMMIDIATE:
+    case LDA_ZERO_PAGE:
+    case LDA_ZERO_PAGE_X:
+    case LDA_ABSOLUTE:
+    case LDA_ABSOLUTE_X:
+    case LDA_ABSOLUTE_Y:
+    case LDA_INDERECT_X:
+    case LDA_INDERECT_Y:
+        cycles_used += cpu_process_LDA(c,r,op_code); break;
+    default:
+        printf("Instruction 0x%x is not implemented yet or undefined. \n", op_code);
+        exit(1);
+        break;
+    }
+
+    c->ps.zero_flag = c->a == 0 ? 1 : 0;
     c->ps.negative_flag = is_bit_set(c->a, 7) ? 1 : 0;
+    return cycles_used;
+}
+
+void cpu_dump_display(cpu* c){
+    printf(":: CPU DUMP:\n");
+    printf("    REGISTERS:\n");
+    printf("        program counter : 0x%04x\n", c->program_counter);
+    printf("        stack ptr       : 0x%02x\n", c->stack_ptr);
+    printf("        register a      : 0x%02x\n", c->a);
+    printf("        register x      : 0x%02x\n", c->ir_x);
+    printf("        register y      : 0x%02x\n", c->ir_y);
+    printf("    PROCESSOR STATUS:\n");
+    printf("        carry           : %d\n", c->ps.carry_flag);
+    printf("        zero            : %d\n", c->ps.zero_flag);
+    printf("        interrupt       : %d\n", c->ps.interrupt_flag);
+    printf("        decimal         : %d\n", c->ps.decimal_flag);
+    printf("        break           : %d\n", c->ps.break_flag);
+    printf("        overflow        : %d\n", c->ps.overflow_flag);
+    printf("        negative        : %d\n", c->ps.negative_flag);
 }
 
 int main(void){
@@ -164,12 +270,19 @@ int main(void){
     printf("Proram counter initialized at: 0x%04x, intialization complete at [%s]\n",
         c->program_counter, c->program_counter == 0x0200 ? "UserSpace" : "Undefined");
 
-    r->data[0x0200] = LDA_ZERO_PAGE;
-    r->data[0x0201] = 0x00;
-    r->data[0x00] =0x45;  
+    r->data[0x0200] = LDA_INDERECT_Y;
+    r->data[0x0201] = 0x44;
+    r->data[0x44] = 0x10;
+    r->data[0x45] = 0x12;
+    
+    r->data[0x1213] = 0x70;
+    
+    //-- hack--
+    c->ir_y = 0x03;
 
-    cpu_execute(c,r,3);
-    printf("0x%x\n",c->a);
+    size_t cu = cpu_execute(c,r);
+    printf("CPU cycles used for execution: %d\n", cu);
+    cpu_dump_display(c);
 
     exit (0);
 }
